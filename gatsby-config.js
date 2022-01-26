f504cd320d5c3c36fdb1d87bf6bbe9528f08fbf2
watanabehideki <hideki.art.2280@gmail.com>
2022-01-27 04:14:08 +0900

    画像の最適化

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  /* 画像最適化の設定 */
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
