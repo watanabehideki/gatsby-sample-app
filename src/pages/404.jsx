bc584137c5798e982df2ac5d077f3b245aef062e
watanabehideki <hideki.art.2280@gmail.com>
2022-01-27 12:21:22 +0900

    404ページを作成

---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div style={{ padding: "20vh 0", textAlign: "center" }}>
+        <h1>Not Found</h1>
+        <h2>お探しのページが見つかりませんでした</h2>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFound
